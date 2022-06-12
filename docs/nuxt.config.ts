d89aae75a186b74ea841285f081a763da51e4097
Sacha STAFYNIAK <sacha@digisquad.io>
2022-06-12 19:06:00 +0200

    wip: update doc content

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   extends: ['./node_modules/@strapi-community/docus-theme'],
+  modules: ['@docus/github'],
+  theme: {},
   github: {
-    repo: 'nuxtlabs/docus-starter'
-  }
+    owner: 'strapi-community',
+    repo: 'strapi-plugin-rest-cache',
+    branch: 'main'
+  },
+
+  // app: {
+  //   baseURL: '/strapi-plugin-rest-cache/',
+  // },
 })
